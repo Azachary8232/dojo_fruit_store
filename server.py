252cc3c1fb2ee316f41e84f1e8f227c2f9dd8787
Azachary8232 <90228565+Azachary8232@users.noreply.github.com>
2021-10-30 16:42:12 -0600

    Fruit Count

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect
-app = Flask(__name__)  
+from flask import Flask, render_template, request, redirect, session
+app = Flask(__name__)
+app.secret_key = "skittles"  
 
 @app.route('/')         
 def index():
@@ -7,7 +8,14 @@ def index():
 
 @app.route('/checkout', methods=['POST'])         
 def checkout():
-    print(request.form)
+    session['strawberry_count'] = request.form['strawberry']
+    session['blackberry_count'] = request.form['blackberry']
+    session['raspberry_count'] = request.form['raspberry']
+    session['apple_count'] = request.form['apple']
+    session['first_name'] = request.form['first_name']
+    session['last_zachary'] = request.form['last_name']
+    session['student_id'] = request.form['student_id']
+    session['count'] = int(session['strawberry_count']) + int(session['blackberry_count']) + int(session['raspberry_count']) + int(session['apple_count'])
     return render_template("checkout.html")
 
 @app.route('/fruits')         
